283dc7678b5a765d20cbfd8f2daadca148a9f111
Vissekopje <78539135+Vissekopje@users.noreply.github.com>
2024-02-12 19:02:57 +0100

    Added functional console tic tac toe game with object oriented design

--- script.js
+++ script.js
@@ -1,38 +1,133 @@
-const game = (function () {
-    let gameBoard = ["", "", "", "", "", "", "", "", ""]
-
-// function Player (name, sign){
- //   this.name = name
-//    this.sign = sign
-// }
 
 
-function addPlayers(player1, player2) {
-    const signPlayerOne = "O"
-    const signPlayerTwo = (signPlayerOne == "O") ? "X" : "O"
-    const playerOneName = player1
-    const playerTwoName = player2
-    return console.log({playerOneName, signPlayerOne, playerTwoName, signPlayerTwo})
+function Cell() {
+    let value = ""
+
+    const addSign = (currentPlayer) => {
+        value = currentPlayer
+    };
+
+    const getValue = () => value
+
+    return{
+        addSign,
+        getValue
+    };
 }
-
-function checkWin(){
-    const WINNINGCOMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
-                                 [0, 3, 6], [1, 4, 7] [2, 5, 8],
-                                 [0, 4, 8], [2, 4, 6]]
-        for (const win of WINNINGCOMBINATIONS){
-            let [a, b, c] = win
-            if (gameBoard[a] && gameBoard[a] == gameBoard[b] && gameBoard[b] == gameBoard[c])
-                return alert(`${gameBoard[a]}`)
+const Board = (function () {
+    let gameBoard = []
+    // create 3 by 3 grid
+    for (let i = 0; i < 3; i++){
+        gameBoard[i] = []
+        for (let j = 0; j < 3; j++){
+            gameBoard[i].push(Cell())
         }
-        return alert("false")
     }
-    return {
-        addPlayers
-    }
-})();
-game.addPlayers("Player 1", "Player 2");
+    const getGameBoard = () => gameBoard;
 
-// gameBoard
-// addSign()
-// updateBoard()
-// checkWin()
+    const putSign = (row, column, player) => {
+        if(gameBoard[row][column].getValue() === ""){
+        gameBoard[row][column].addSign(player)
+        game.checkWin()
+    }
+    else {
+        console.log("Cell is full")
+    }
+}
+    const printBoard = () => {
+        const boardWithCellValues = gameBoard.map((row) => row.map((cell) => cell.getValue()))
+        console.log(boardWithCellValues);
+      }
+
+    const clearBoard = () => {
+        for (let i = 0; i < 3; i++){
+            gameBoard[i] = []
+            for (let j = 0; j < 3; j++){
+                gameBoard[i][j] = Cell()
+            }
+        }
+        printBoard()
+    }
+
+     return { 
+        getGameBoard,
+        putSign,
+        printBoard,
+        clearBoard
+     }
+
+})();
+
+
+
+
+ //   function addPlayers(player1, player2) {
+   //     const signPlayerOne = "O"
+     //   const signPlayerTwo = (signPlayerOne == "O") ? "X" : "O"
+     //   const playerOne = new Player (player1, signPlayerOne)
+     //   const playerTwo = new Player (player2, signPlayerTwo)
+     //   Board.printBoard()
+     //   return console.log({playerOne, playerTwo})
+   //  }
+  
+
+   const game = (function () {
+    const gameBoard = Board.getGameBoard();
+
+    function Player(name, sign){
+        this.name = name;
+        this.sign = sign;
+    }
+    let playerOne = new Player("Player 1", "O");
+    let playerTwo = new Player("Player 2", "X");
+    let currentPlayer = playerOne
+
+    function switchPlayers() {
+        currentPlayer = (currentPlayer === playerOne) ? playerTwo : playerOne;
+    }
+
+    const getCurrentPlayer = () => currentPlayer;
+
+    const newRound = () => {
+        Board.printBoard();
+        console.log(`${getCurrentPlayer().name}'s turn`);
+    };
+
+    const playRound = (row, column) => {
+        console.log(
+            `Putting ${getCurrentPlayer().name}'s sign into row ${row} and column ${column}`
+        );
+        Board.putSign(row, column, getCurrentPlayer().sign);
+
+        switchPlayers();
+        newRound();
+    };
+
+    function checkWin() {
+        const WINNINGCOMBINATIONS = [
+        [[0,0],[1,1],[2,2]], 
+        [[0,0],[1,0],[2,0]],
+        [[0,1],[1,1],[2,1]],
+        [[0,2],[1,2],[2,2]], 
+        [[0,0],[0,1],[0,2]], 
+        [[1,0],[1,1],[1,2]],
+        [[2,0],[2,1],[2,2]], 
+        [[0,2],[1,1],[2,0]]
+    ];
+        for (const win of WINNINGCOMBINATIONS) {
+            let [[a, b],[c, d],[e, f]] = win;
+            if (gameBoard[a][b].getValue() !== "" 
+            && gameBoard[a][b].getValue() === gameBoard[c][d].getValue() 
+            && gameBoard[a][b].getValue() === gameBoard[e][f].getValue()) {
+                Board.clearBoard()
+                return alert(`${getCurrentPlayer().name} wins!`);
+            }
+        }
+        return alert("No winner yet!");
+    }
+
+    return {
+        checkWin,
+        playRound
+    };
+})();
